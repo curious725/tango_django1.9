87f8cff22d08ac328d05d0c72bcba06c9e313b6b
curious725 <curious725@gmail.com>
2017-07-26 14:11:16 +0300

    configure django-debug-toolbar

--- tango_with_django_project/tango_with_django_project/settings/dev.py
+++ tango_with_django_project/tango_with_django_project/settings/dev.py
@@ -5,3 +5,17 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = get_secret("dev_allowed_hosts")
+
+
+MIDDLEWARE_CLASSES += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+INSTALLED_APPS += ['debug_toolbar', ]
+
+INTERNAL_IPS = get_secret("django_debug_toolbar_internal_ips")
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'DISABLE_PANELS': [
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ],
+    'SHOW_TEMPLATE_CONTEXT': True,
+}
