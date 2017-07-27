6beb08b29ec0f00afcd9fb0608a21f9783f5e765
curious725 <curious725@gmail.com>
2017-07-27 12:20:18 +0300

    create about view()

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,3 +6,9 @@ def index(request):
     return HttpResponse(
         "Rango says hey there partner"
     )
+
+
+def about(request):
+    return HttpResponse(
+        "Rango says here is the about page."
+    )
