16d4d8711ec13363e73a83c7fd12960093050c82
curious725 <curious725@gmail.com>
2017-07-28 13:38:16 +0300

    configure static files in settings

--- tango_with_django_project/tango_with_django_project/settings/base.py
+++ tango_with_django_project/tango_with_django_project/settings/base.py
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 )
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # JSON-based secrets module
 with open(os.path.join(BASE_DIR, '..', 'secrets.json')) as f:
@@ -152,3 +153,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
