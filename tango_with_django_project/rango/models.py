bfdd1c1bedae600d2e7932f12a60b81181ccf245
curious725 <curious725@gmail.com>
2017-07-29 13:56:13 +0300

    add Page model

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -8,3 +8,13 @@ class Category(models.Model):
 
     def __unicode__(self):  # for Python 3, use __str__
         return self.name
+
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __unicode__(self):  # for Python3, use __str__
+        return self.title
