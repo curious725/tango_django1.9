b6fa42d00b4629556db5cde425244c0e1adb688b
curious725 <curious725@gmail.com>
2017-07-29 13:53:17 +0300

    add Category model

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __unicode__(self):  # for Python 3, use __str__
+        return self.name
