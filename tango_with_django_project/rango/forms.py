48f84327b90ad08840ee8d3022c0a60619ff1966
curious725 <curious725@gmail.com>
2017-08-02 12:16:13 +0300

    add CategoryForm

---
+++ tango_with_django_project/rango/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Category, Page
+
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(
+        max_length=128,
+        help_text="Please enter the category name."
+    )
+    views = forms.IntegerField(
+        widget=forms.HiddenInput(), initial=0
+    )
+    likes = forms.IntegerField(
+        widget=forms.HiddenInput(), initial=0
+    )
+    slug = forms.CharField(
+        widget=forms.HiddenInput(), required=False
+    )
+
+    # An inline class to provide additional information on the form.
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Category
+        fields = ('name',)
