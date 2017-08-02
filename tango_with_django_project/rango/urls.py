e973693b390c4743bb094dcb8a4b31bfee9f0ae5
curious725 <curious725@gmail.com>
2017-08-02 13:16:40 +0300

    map url to add_category() view

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     url('^$', views.index, name='index'),
     # ex. /rango/about
     url('^about/$', views.about, name='about'),
+    # ex. /rango/add_category
+    url('^add_category/$', views.add_category, name='add_category'),
     # ex. /rango/category/python
     url('^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
