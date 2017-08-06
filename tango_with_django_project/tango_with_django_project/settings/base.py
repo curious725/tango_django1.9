0f7826bc1e1a117b0adcd49f8e9980cdbf814cf2
curious725 <curious725@gmail.com>
2017-08-06 15:56:07 +0300

    tweak custom validator to appove passwords only with 6 characters length

--- tango_with_django_project/tango_with_django_project/settings/base.py
+++ tango_with_django_project/tango_with_django_project/settings/base.py
@@ -124,6 +124,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'OPTIONS': {'min_length': 6, },
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
