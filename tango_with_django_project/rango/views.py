407877ef1e5b0a022303e0b89cc1fd290483492f
curious725 <curious725@gmail.com>
2017-08-08 13:37:21 +0300

    add logout view

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
@@ -227,6 +227,16 @@ def user_login(request):
         return render(
             request, 'rango/login.html', {}
         )
+
+
+# Use the @login_required() decorator to ensure only those
+# logged in can access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # take the user back to the homepage.
+    return HttpResponseRedirect(reverse('rango:index'))
 
 
 @login_required
