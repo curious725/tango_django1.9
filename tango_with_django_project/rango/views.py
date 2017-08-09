f043a0bc4fe8616b9ed565405659a59c56d73aea
curious725 <curious725@gmail.com>
2017-08-09 10:31:22 +0300

    restricted() view uses template now

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -243,6 +243,6 @@ def user_logout(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse(
-        "Since you're logged in, you can see this text"
+    return render(
+        request, 'rango/restricted.html', {}
     )
