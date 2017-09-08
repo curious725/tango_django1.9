eb9d3021a8c2f60c32b5e62512dadf41a2a53c78
curious725 <curious725@gmail.com>
2017-09-08 18:56:04 +0300

    add django-registration-redux in installed apps in settings

--- tango_with_django_project/tango_with_django_project/settings/base.py
+++ tango_with_django_project/tango_with_django_project/settings/base.py
@@ -59,6 +59,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rango.apps.RangoConfig',
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
