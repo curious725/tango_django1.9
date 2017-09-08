6aaec295a94e6dc923cd19d6828faf616d5ac6ed
curious725 <curious725@gmail.com>
2017-09-08 17:01:46 +0300

    write site counter example with client side cookies

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -20,9 +22,11 @@ def index(request):
     page_most_viewed = Page.objects.order_by('-views')[:5]
     context_dict = {'categories_most_liked': category_most_liked,
                     'page_most_viewed': page_most_viewed}
-    return render(
+    response = render(
         request, 'rango/index.html', context=context_dict
     )
+    visitor_cookie_handler(request, response)
+    return response
 
 
 def about(request):
@@ -246,3 +250,31 @@ def restricted(request):
     return render(
         request, 'rango/restricted.html', {}
     )
+
+
+def visitor_cookie_handler(request, response):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get(
+        'last_visit', str(datetime.now())
+    )
+    last_visit_time = datetime.strptime(
+        last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S'
+    )
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # update the last visit cookie now that we have updated the count
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        visits = 1
+        # set the last visits cookie
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    # Update/set the visits cookie
+    response.set_cookie('visits', visits)
