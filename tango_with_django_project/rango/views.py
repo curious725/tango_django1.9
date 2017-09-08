0d01cd600b197b48f40cfd916a01f23289ae8c45
curious725 <curious725@gmail.com>
2017-09-08 17:20:54 +0300

    update the visitor cookie handler function

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -260,15 +260,12 @@ def get_server_side_cookie(request, cookie, default_val=None):
     return val
 
 
-def visitor_cookie_handler(request, response):
-    # Get the number of visits to the site.
-    # We use the COOKIES.get() function to obtain the visits cookie.
-    # If the cookie exists, the value returned is casted to an integer.
-    # If the cookie doesn't exist, then the default value of 1 is used.
-    visits = int(request.COOKIES.get('visits', '1'))
-
-    last_visit_cookie = request.COOKIES.get(
-        'last_visit', str(datetime.now())
+def visitor_cookie_hanlder(request):
+    visits = int(get_server_side_cookie(
+        request, 'visits', 1
+    ))
+    last_visit_cookie = get_server_side_cookie(
+        request, 'last_visit', str(datetime.now())
     )
     last_visit_time = datetime.strptime(
         last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S'
@@ -278,11 +275,11 @@ def visitor_cookie_handler(request, response):
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
         # update the last visit cookie now that we have updated the count
-        response.set_cookie('last_visit', str(datetime.now()))
+        request.session['last_visit'] = str(datetime.now())
     else:
         visits = 1
-        # set the last visits cookie
-        response.set_cookie('last_visit', last_visit_cookie)
+        # set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
 
-    # Update/set the visits cookie
-    response.set_cookie('visits', visits)
+    # update/set the visits cookie
+    request.session['visits'] = visits
